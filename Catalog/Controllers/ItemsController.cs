5ed8945b93045dd90b4789d5f5947bbf040f79a3
batuhanOnur <batuhanonur29@gmail.com>
2021-09-14 21:03:14 +0300

    Dtos

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.DTOs;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+
+            return items;
+        }
+
+        // GET /items/id
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id) //actionresult 1den fazla tip dönmeyi sağlar.
+        {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; } // sadece init'de değer alır. yaratıldıktan sonra değer alamaz.
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+
+    }
+}
